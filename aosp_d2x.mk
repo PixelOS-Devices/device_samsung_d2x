89a7e67a69ea58aaab6e69df99441706e834dc73
Jubscleisson <fcodl0706@gmail.com>
2023-04-22 13:47:05 -0300

    d2x: Inherit common_full_phone
    - This is causing missing apns in the system, causing mobile data malfunction.
    - The bringup of LineageOS to PixelOS was made at a time when PixelOS was in Beta and used common.mk due to the ArrowOS base, but a while later, this started to use common_full_phone.mk, and ended up causing the "break" of mobile data.
    
    Signed-off-by: Jubscleisson <fcodl0706@gmail.com>

--- aosp_d2x.mk
+++ aosp_d2x.mk
@@ -38,7 +38,7 @@ TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common build stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
